4563de4f82dff82e2eea97574e8f9efa62582e99
Roberto <p.roberto@wustl.edu>
2022-04-14 14:01:38 -0500

    update2

--- header.js
+++ header.js
@@ -1,46 +1,19 @@
-(function($) { "use strict";
-
-  $(function() {
-    var header = $(".start-style");
-    $(window).scroll(function() {    
-      var scroll = $(window).scrollTop();
-    
-      if (scroll >= 10) {
-        header.removeClass('start-style').addClass("scroll-on");
-      } else {
-        header.removeClass("scroll-on").addClass('start-style');
-      }
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
     });
-  });   
-    
-  //Animation
-  
-  $(document).ready(function() {
-    $('body.hero-anime').removeClass('hero-anime');
-  });
+});
 
-  //Menu On Hover
-    
-  $('body').on('mouseenter mouseleave','.nav-item',function(e){
-      if ($(window).width() > 750) {
-        var _d=$(e.target).closest('.nav-item');_d.addClass('show');
-        setTimeout(function(){
-        _d[_d.is(':hover')?'addClass':'removeClass']('show');
-        },1);
-      }
-  }); 
-  
-  //Switch light/dark
-  
-  $("#switch").on('click', function () {
-    if ($("body").hasClass("dark")) {
-      $("body").removeClass("dark");
-      $("#switch").removeClass("switched");
-    }
-    else {
-      $("body").addClass("dark");
-      $("#switch").addClass("switched");
-    }
-  });  
-  
-  })(jQuery); 
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
