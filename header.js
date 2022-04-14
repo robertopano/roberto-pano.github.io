bc41e7d51ff15aea457834c0063f3f6b3a40693b
Roberto <p.roberto@wustl.edu>
2022-04-14 17:35:19 -0500

    update

--- header.js
+++ header.js
@@ -4,6 +4,3 @@ let sign = prompt("Would you like to play a game?");
 if (sign.toLowerCase() == "yes") {
     alert("Ok! Solve this puzzle");
 }
-
-// there are many ways to use the prompt feature
-sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
