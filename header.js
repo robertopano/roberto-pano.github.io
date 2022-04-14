e4de81f99ceb6cbc23a354efa8d3eb29cb437f80
Roberto <p.roberto@wustl.edu>
2022-04-14 17:34:16 -0500

    update

--- header.js
+++ header.js
@@ -6,7 +6,4 @@ if (sign.toLowerCase() == "yes") {
 }
 
 // there are many ways to use the prompt feature
-sign = window.prompt(); // open the blank prompt window
-sign = prompt();       //  open the blank prompt window
 sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
-sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with Text "Are you feeling lucky" and default value "sure"
