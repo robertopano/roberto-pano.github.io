68f218d9f6bc4fb0c4daef8f3c3fa49760339aca
Roberto <p.roberto@wustl.edu>
2022-04-14 17:29:50 -0500

    update

--- header.js
+++ header.js
@@ -1,14 +1,12 @@
-function askName() {
-    let username = sessionStorage.getItem('username');
+  
+let sign = prompt("Would you like to play a game?");
 
-    if (username === null) {
-        username = prompt("To make your time on this website better, please enter your name.");
-    }
-
-    if (username != null) {
-        document.getElementById("userpara").innerHTML = "Hello, " + username;
-        sessionStorage.setItem('username', username);
-    } else {
-        document.getElementById("noUser").innerHTML = "Welcome, Stranger!";
-    }
+if (sign.toLowerCase() == "yes") {
+    alert("Ok! Solve this puzzle");
 }
+
+// there are many ways to use the prompt feature
+sign = window.prompt(); // open the blank prompt window
+sign = prompt();       //  open the blank prompt window
+sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
+sign = window.prompt('Are you feeling lucky', 'sure'); // open the window with Text "Are you feeling lucky" and default value "sure"
