c462a7da487fefeb306437545f0a31171946add8
Roberto <p.roberto@wustl.edu>
2022-11-02 01:01:01 -0500

    Update
    Changed main page and added project button

--- header.js
+++ header.js
@@ -1,6 +1,16 @@
   
-let sign = prompt("Would you like to play a game?");
+let sign = prompt("Are you having a great day?");
 
 if (sign.toLowerCase() == "yes") {
-    alert("Ok! Solve this puzzle");
+    alert("Great! Glad to here :)");
 }
+else{
+    alert("I am sorry to hear that, I hope it gets better!")
+}
+
+
+function swe(event){
+    location.href = "http://ec2-50-16-177-186.compute-1.amazonaws.com/~roberto/"
+}
+
+document.getElementById("swe_button").addEventListener("swe", share, false);
