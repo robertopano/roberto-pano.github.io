0578ad8cb9946ff14d6f672b94852a437d52f630
Roberto <p.roberto@wustl.edu>
2022-11-02 01:08:21 -0500

    prompting update

--- header.js
+++ header.js
@@ -1,16 +1,23 @@
-  
-let sign = prompt("Are you having a great day?");
+var response = "";
+if(response = ""){
+    let sign = prompt("Are you having a great day?");
 
-if (sign.toLowerCase() == "yes") {
-    alert("Great! Glad to here :)");
+    if (sign.toLowerCase() == "yes") {
+        alert("Great! Glad to here :)");
+        response = "yes"
+    }
+    else if (sign.toLowerCase() == "no"){
+        alert("I am sorry to hear that, I hope it gets better!")
+        response = "no";
+    }
+    else{
+        alert("No worries!")
+        response = "none";
+    }
 }
-else{
-    alert("I am sorry to hear that, I hope it gets better!")
-}
-
 
 function swe(event){
     location.href = "http://ec2-50-16-177-186.compute-1.amazonaws.com/~roberto/"
-};
+}
 
 document.getElementById("swe_button").addEventListener("swe", share, false);
